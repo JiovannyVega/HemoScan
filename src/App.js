a33715c76d6efd649251591a90612932ccf90e7b
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-10-06 22:14:32 -0600

    Se traduce a react una de las paginas en html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+          <img src={require('./assets/logo.png')} alt="HemoScan AI" />
+          <h1>HemoScan AI</h1>
+        </div>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Acerca de</a></li>
+          <li><a href="#" className="active">Acceder</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function LoginContainer() {
+  return (
+    <main>
+      <div className="login-container">
+        <div className="form-container">
+          <img
+            src={require('./assets/perfil-icono.png')}
+            alt="Profile Icon"
+            className="profile-icon"
+          />
+          <form>
+            <label htmlFor="email">Correo electrónico</label>
+            <input type="email" id="email" placeholder="Correo electrónico" />
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" id="password" placeholder="Contraseña" />
+            <a href="#" className="forgot-password">¿Olvidaste tu contraseña?</a>
+            <button type="submit">Ingresar</button>
+          </form>
+          <p>O</p>
+          <button className="google-login">
+            <img src={require('./assets/google.png')} alt="Google" /> Continuar con Google
+          </button>
+          <p>
+            ¿No tienes una cuenta?
+            <a href="#" className="register-link">Regístrate</a>
+          </p>
+        </div>
+        <div className="image-container">
+          <img src={require('./assets/medicos.png')} alt="People Working" />
+        </div>
+      </div>
+    </main>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <LoginContainer />
     </div>
   );
 }
