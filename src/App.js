bc4b7b193a51bf1dcb72d5ab3e2d4bcb97abfe10
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-10-07 15:56:46 -0600

    Se reestructura el codigo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import LoginPage from './LoginPage.js';
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-
-function Header() {
-  return (
-    <header>
-      <nav>
-        <div className="logo">
-          <img src={require('./assets/logo.png')} alt="HemoScan AI" />
-          <h1>HemoScan AI</h1>
-        </div>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">Acerca de</Link></li>
-          <li><Link to="/login" className="active">Acceder</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Header from "./components/Header.js";
+import LoginPage from "./pages/LoginPage.js";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage.js";
+import "./App.css"; // Import the CSS file
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+          <img src={require("../assets/logo.png")} alt="HemoScan AI" />
+          <h1>HemoScan AI</h1>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Acerca de</Link>
+          </li>
+          <li>
+            <Link to="/login" className="active">
+              Acceder
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
