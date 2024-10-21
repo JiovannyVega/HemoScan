4d59af438074843a4a8bdc1ba454e0db6e9676d3
NestorEsk <nestoryahir2003@gmail.com>
2024-10-21 14:28:51 -0600

    Correccion de colores en HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,13 +11,13 @@ const HomePage = () => {
                     <div>
                         <p class="text-xl mb-5 text-white text-center mt-4">Obten resultados precisos de tus examenes de hemoglobina y recomendaciones personalizadas al instante.</p>
                         <div class="flex justify-center">
-                            <Link to="" class="m-2 p-4 rounded-md cursor-pointer bg-sky-950 text-white">Iniciar Sesion</Link>
-                            <Link to="" class="m-2 p-4 rounded-md cursor-pointer bg-sky-950 text-white">Crear Cuenta</Link>
+                            <Link to="" class="m-2 p-4 rounded-md cursor-pointer bg-primary hover-secondary text-white">Iniciar Sesion</Link>
+                            <Link to="" class="m-2 p-4 rounded-md cursor-pointer bg-primary hover-secondary text-white">Crear Cuenta</Link>
                         </div>
                     </div>
                 </section>
                 <div class="flex flex-col bg-gray-100 m-0 p-0 w-4/5 items-center">
-                    <section class="text-center w-3/5 mt-20 text-black">
+                    <section class="text-center w-3/5 mt-20 text-text">
                         <p class="text-4xl mt-6">Bienvenido a HemoScan</p>
                         <p class="text-2xl mt-6">Analiza tus examenes de hemoglobina y obtén resultados rápidos y recomendacionales personalizadas basadas en inteligencia artificial</p>
                     </section>
@@ -26,7 +26,7 @@ const HomePage = () => {
                         <p class="text-2xl mt-6">HemoScan utiliza inteligencia artificial avanzada para analizar tus exámenes médicos y proporcionarte resultados precisos y recomendaciones para mejorar tu salud</p>
                         <button class="m-2 p-4 mt-6 rounded-md cursor-pointer bg-sky-950 text-white">Subir Examen</button>
                     </section>
-                    <section class="w-3/5 mt-18 text-black">
+                    <section class="w-3/5 mt-18 text-text">
                         <p class="text-center text-4xl mt-6">¿Por qué elegir HemoScan?</p>
                         <ul class="text-left list-none p-0 text-2xl mt-6 mb-20">
                             <li>Rápido, preciso y seguro</li>
