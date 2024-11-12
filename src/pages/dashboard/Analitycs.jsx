1ba705658e5c37ed04074d73f3800d358447959d
NestorEsk <nestoryahir2003@gmail.com>
2024-11-12 14:18:25 -0600

    Creación de graficos avanzados para la pestaña de analisis

--- src/pages/dashboard/Analitycs.jsx
+++ src/pages/dashboard/Analitycs.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement } from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-
+ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+const superior = 5.28
+const inferior = 4.69
 const Analitycs = () => {
   const data = {
-    labels: ['Eritrocitos', 'Hemoglobina', 'Hematocrito', 'MCV', 'MCH', 'MCHC', 'RDW'],
+    labels: ['Examen1', 'Examen2', 'Examen3', 'Examen4', 'Examen5', 'Examen6', 'Examen7'], // Etiquetas en el eje X
     datasets: [
       {
-        label: 'Formula roja',
-        data: [4.03, 11.8, 35.4, 87.8, 29.3, 33.3, 12.9],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
+        label: 'Eritrocitos',
+        data: [4.03, 4.01, 4.05, 5, 3.99, 3.3, 4.7], // Valores en el eje Y
+        fill: false, // No rellenar el área debajo de la línea
+        borderColor: 'rgba(75, 192, 192, 1)', // Color de la línea
+        borderWidth: 2, // Ancho de la línea
+        tension: 0.1, // Curvatura de la línea (0 para línea recta)
+        // Si no quieres mostrar los puntos, simplemente omite cualquier propiedad relacionada con los puntos
+      },
+      {
+        label: 'Rango superior',
+        data: [superior, superior, superior, superior, superior, superior, superior], // Valores constantes que definen el límite superior
+        fill: false,
+        borderColor: 'rgba(255, 99, 132, 0.6)', // Color de la línea superior
         borderWidth: 1,
+        borderDash: [5, 5], // Líneas discontinuas
+      },
+      // Línea inferior de rango
+      {
+        label: 'Rango inferior',
+        data: [inferior, inferior, inferior, inferior, inferior, inferior, inferior], // Valores constantes que definen el límite inferior
+        fill: false,
+        borderColor: 'rgba(255, 159, 64, 0.6)', // Color de la línea inferior
+        borderWidth: 1,
+        borderDash: [5, 5], // Líneas discontinuas
       },
     ],
   };
@@ -26,10 +46,25 @@ const Analitycs = () => {
       },
       title: {
         display: true,
-        text: 'Hemoglobina',
+        text: 'Eritrocitos',
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Indicadores',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Valores',
+        },
       },
     },
   };
+
   return (
     <>
       <div className='h-full p-8 border-t-2 bg-background dark:bg-background-dark'>
@@ -37,7 +72,7 @@ const Analitycs = () => {
           <div className='flex-col w-1/2 mx-5 border shadow-xl rounded-xl h-1/2'>
             <div className="flex items-center justify-center h-full rounded-xl">
               <div className="w-full h-full p-4 mx-auto">
-                <Bar data={data} options={options} />
+                <Line data={data} options={options} />
               </div>
             </div>
           </div>
